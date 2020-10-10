8bd26af3a24ea2dda894fa1473a47f8169417c20
unknown <ayush1995shrivastava@gmail.com>
2020-10-10 13:31:12 +0530

    Consumer Service Files

---
+++ src/main/java/com/example/consumerfacingservice/ConsumerFacingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumerfacingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerFacingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFacingServiceApplication.class, args);
+	}
+
+}
